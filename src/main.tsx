c4fb67360a99738faa4fe4f2d842c4e2e3d358fb
Redian Fikri <redianmf@gmail.com>
2026-03-25 13:52:22 +0700

    feat: initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/global.css";
 import App from "./App.tsx";
-import { TooltipProvider } from "@/components/ui/too";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import { Button } from "@/components/ui/button";
 
 createRoot(document.getElementById("root")!).render(
