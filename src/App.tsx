96988953d470116f1591c7365487786a08adcf47
Redian Fikri <redianmf@gmail.com>
2026-03-25 10:50:10 +0700

    feat: setup config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
     <>
       <div className="bg-red-300">Hello world</div>
+      <Button>Button</Button>
     </>
   );
 }
